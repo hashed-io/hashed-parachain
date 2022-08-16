86283a280b17b5a96dd818deb9a39790b0f8324a
3yekn <3yekn@users.noreply.github.com>
2022-08-16 19:44:01 +0000

    deploy: 1028ebc4d0495a2ea65091012418f24a361f08bd

--- implementors/sc_chain_spec/extension/trait.Fork.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["hashed_parachain"] = [{"text":"impl Fork for <a class=\"struct\" href=\"hashed_parachain/chain_spec/struct.ExtensionsFork.html\" title=\"struct hashed_parachain::chain_spec::ExtensionsFork\">ExtensionsFork</a>","synthetic":false,"types":["hashed_parachain::chain_spec::ExtensionsFork"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var sourcesIndex = {};
-sourcesIndex["hashed_parachain"] = {"name":"","files":["chain_spec.rs","cli.rs","command.rs","main.rs","rpc.rs","service.rs"]};
+sourcesIndex["hashed_parachain"] = {"name":"","dirs":[{"name":"chain_spec","files":["hashed.rs","luhn.rs","md5.rs","mod.rs"]}],"files":["cli.rs","command.rs","main.rs","rpc.rs","service.rs"]};
 sourcesIndex["hashed_parachain_runtime"] = {"name":"","dirs":[{"name":"weights","files":["block_weights.rs","extrinsic_weights.rs","mod.rs","paritydb_weights.rs","rocksdb_weights.rs"]}],"files":["constants.rs","lib.rs","xcm_config.rs"]};
 sourcesIndex["pallet_bitcoin_vaults"] = {"name":"","files":["functions.rs","lib.rs","types.rs"]};
 sourcesIndex["pallet_fruniques"] = {"name":"","files":["functions.rs","lib.rs"]};
